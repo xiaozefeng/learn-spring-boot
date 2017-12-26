e5aef0be6c99f8e4c09e831e47a00ea5d96c11ed
肖泽锋 <qq523107430@163.com>
2017-12-27 00:12:55 +0800

    初始化

---
+++ src/main/java/com/gz/learnspringboot/LearnSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.gz.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaozefeng
+ */
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+}
