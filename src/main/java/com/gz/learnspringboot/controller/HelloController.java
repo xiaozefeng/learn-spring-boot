b8c76f1c154af278036c9ac8aafafcc592f6ed98
肖泽锋 <qq523107430@163.com>
2017-12-28 23:19:36 +0800

    自定义starter

---
+++ src/main/java/com/gz/learnspringboot/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.gz.learnspringboot.controller;
+
+import com.gz.learnspringboot.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiaozefeng
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return helloService.sayHello();
+    }
+}
